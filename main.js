1bb45523e5fd3ca1074c4c87e76a52fef4700523
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-02-06 17:41:59 +0400

    edit button added

--- main.js
+++ main.js
@@ -46,6 +46,34 @@ function deleteUser(id) {
 };
 // -----elave olunan datanı silmek----END--
 
+// -----elave olunan datanı edit etmek---START---
+
+
+function editUser(button, id) {
+    const {firstElementChild, lastElementChild, previousElementSibling} = button.parentElement.previousElementSibling
+    let passwordSpan = firstElementChild;
+    let passwordInput = lastElementChild;
+
+    let emailSpan = previousElementSibling.firstElementChild;
+    let emailInput = previousElementSibling.lastElementChild;
+
+    let usernameSpan = previousElementSibling.previousElementSibling.firstElementChild;
+    let usernameInput = previousElementSibling.previousElementSibling.lastElementChild;
+
+    passwordSpan.classList.add("d-none");
+    passwordInput.classList.remove("d-none");
+
+    emailSpan.classList.add("d-none");
+    emailInput.classList.remove("d-none");
+
+    usernameSpan.classList.add("d-none");
+    usernameInput.classList.remove("d-none");
+
+    
+}
+
+// -----elave olunan datanı edit etmek---END---
+
 // ---yazılan datanın elave olunması----START--
 function addUi(param) {
     tBodyElem.innerHTML = "";
@@ -54,16 +82,26 @@ function addUi(param) {
         tBodyElem.innerHTML+= `
         <tr>
                   <td>${id}</td>
-                  <td>${fullname}</td>
-                  <td>${email}</td>
-                  <td>*********</td>
+                  <td class="userNameCol"> 
+                  <span>${fullname}</span>
+                  <input type="email" class="d-none">
+                  </td>
+                  <td class="userEmailCol">
+                   <span>${email}</span>
+                  <input type="email" class="d-none">
+                  </td>
+                  <td class="userPassCol">
+                  <span>*********</span>
+                  <input type="password" class="d-none">
+                  </td>
                   <td>
                     <button class="btn btn-danger" onclick="deleteUser(${id})">Silmək</button>
-                    <button class="btn btn-secondary">Düzəltmək</button>
+                    <button class="btn btn-secondary" onclick="editUser(this,${id})">Düzəltmək</button>
                   </td>
         </tr>
         `
     })
 }
 // ---yazılan datanın elave olunması----END--
+
 
