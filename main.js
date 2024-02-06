b3b5b400a3635d19652a69cd3c323d45500129bc
nicatverdiyev <nicatverdiyev101@gmail.com>
2024-02-06 16:24:13 +0400

    delete button added

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ let formElem = document.querySelector("form");
 const tBodyElem = document.querySelector("table tbody");
 let users =[];
 let userId = 0;
+let localVal = JSON.parse(localStorage.getItem("users"));
+let localId = localStorage.getItem("userId");
+if (localVal) {
+    users =  localVal;
+    addUi(users)
+}
+if (localId) {
+    userId = localId;
+}
+
 formElem.addEventListener("submit", (e) =>{
     e.preventDefault();
     let foundUser= users.find(user => user.email === useremail.value);
@@ -14,26 +24,46 @@ formElem.addEventListener("submit", (e) =>{
                 pass:userpass.value
             });
             addUi(users);
+            localStorage.setItem("users",JSON.stringify(users));
+            localStorage.setItem("userId",userId);
 
     }else{
-        alert("asasas");
+        alert("e-poçt artıq elave edilib");
     }
+
+   fullname.value = "";
+   useremail.value = "";
+    userpass.value = "";
 });
 
+// -----elave olunan datanı silmek----START--
+function deleteUser(id) {
+    users = users.filter(user=> user.id !== id);
+    localStorage.setItem("users",JSON.stringify(users));
+
+    addUi(users);
+    
+};
+// -----elave olunan datanı silmek----END--
+
+// ---yazılan datanın elave olunması----START--
 function addUi(param) {
+    tBodyElem.innerHTML = "";
     param.forEach(user =>{
-        const {id,fullName, email,pass} = user
+        const {id,fullname, email,pass} = user
         tBodyElem.innerHTML+= `
         <tr>
                   <td>${id}</td>
-                  <td>${fullName}</td>
+                  <td>${fullname}</td>
                   <td>${email}</td>
                   <td>*********</td>
                   <td>
-                    <button class="btn btn-danger">Silmək</button>
+                    <button class="btn btn-danger" onclick="deleteUser(${id})">Silmək</button>
                     <button class="btn btn-secondary">Düzəltmək</button>
                   </td>
         </tr>
         `
     })
 }
+// ---yazılan datanın elave olunması----END--
+
